eb6f4958ebf33cf8f8404a3d6a6dc5e0db851d7f
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-17 22:37:42 -0300

    added Pacientes at model

--- src/main/java/br/com/fiap/checkpoint2/model/Paciente.java
+++ src/main/java/br/com/fiap/checkpoint2/model/Paciente.java
@@ -1,5 +1,38 @@
 package br.com.fiap.checkpoint2.model;
 
-public class Paciente {
+import java.sql.Timestamp;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pacientes")
+public class Paciente extends AbstractEntity<Long> {
+	
+	@Column(nullable = false, length = 50)
+	private String nome;
+	
+	@Column(nullable = false, length = 50)
+	private String endereco;
+	
+	@Column(nullable = false, length = 50)
+	private String bairro;
+	
+	@Column(nullable = false, length = 50)
+	private String email;
+	
+	@Column(nullable = false, length = 50)
+	private String telefone_completo;
+	
+	@Column(nullable = false)
+	@CreationTimestamp
+	private Timestamp created_at;
+	
+	@Column(nullable = false)
+	@CreationTimestamp
+	private Timestamp updated_at;
+	
 }
